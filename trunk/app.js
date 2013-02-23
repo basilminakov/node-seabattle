8389d6a3f508a465fbb510f6bfb85219d16c6ade
basilminakov@gmail.com <basilminakov@gmail.com@0f2923f0-4e49-2880-5c77-176d88a3c168>
2013-02-23 14:02:30 +0000

    Reorganise source code

--- trunk/app.js
+++ trunk/app.js
@@ -5,11 +5,11 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
 var app = express();
+
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -27,8 +27,44 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
+var gameData = require('./public/js/game.js'),
+info = require('./public/js/informer.js');
+var world = new gameData.World();
+var gameServ = new gameData.GameServer();
+
 app.get('/', routes.index);
-app.get('/users', user.list);
+
+app.get('/beerkoding/shoot', function(req, res){
+    var p = req.query;
+    if ((p.x < 0 || p.x >=230) || (p.y < 0 || p.y >=230)) {
+        res.send(0);
+        return;
+    }
+    var ip = req.ip;
+    var result = world.we.shot(world.enemy[ip], p.x, p.y);
+    res.send(result);
+});
+
+app.get('/beerkoding/spy', function(req, res){
+    if (world.we.isAlive) {
+        var p = req.query;
+        res.send(1);
+    } else {
+        res.send(3);
+    }
+});
+
+app.get('/map', function(req, res) {
+    info.showMap(req, res, world.we.field);
+});
+
+app.get('/status', function(req, res) {
+    routes.status(req, res, world.we);
+});
+
+app.get('/enemies', function(req, res) {
+    routes.enemies(req, res, world.they);
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
