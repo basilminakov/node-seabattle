f08d8da6a593e129aea91331ceea0a51e45b2c66
basilminakov@gmail.com <basilminakov@gmail.com@0f2923f0-4e49-2880-5c77-176d88a3c168>
2013-02-24 08:24:25 +0000

    Enemy statistics has been added

--- trunk/routes/index.js
+++ trunk/routes/index.js
@@ -1,20 +1,27 @@
 
-/*
- * GET home page.
- */
-
 exports.index = function(req, res){
-	res.render('index', { title: 'Main page' });
+	res.render('index', {  
+		title: 'Main page' 
+	});
 };
 
 exports.status = function(req, res, we) {
-	res.render('status', { title: 'Game status', we: we });
+	res.render('status', {  
+		title: 'Game status', 
+		we: we 
+	});
 };
 
 exports.enemies = function(req, res, they) {
-	res.render('enemies', { title: 'Enemies status', they: they });
+	res.render('enemies', {  
+		title: 'Enemies status', 
+		they: they 
+	});
 };
 
 exports.map = function(req, res, field) {
-	res.render('map', { title: 'Battleships location', data: field });
+	res.render('map', { 
+		title: 'Battleships location', 
+		data: field 
+	});
 };
